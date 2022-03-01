4e32b8744108cec0b770ddb6c90c0515a29a05fc
Sereny Sanchez <52845840+SerenyS@users.noreply.github.com>
2022-02-28 19:16:06 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import Country from './components/Country';
+import NewCountry from './components/NewCountry';
+import './App.css';
+
+const App = () => {
+  const apiEndpoint = "https://serenysmedalsapi.azurewebsites.net/api/country";
+  
+  const [ countries, setCountries ] = useState([]);
+  const medals = useRef([
+    { id: 1, name: 'gold' },
+    { id: 2, name: 'silver' },
+    { id: 3, name: 'bronze' },
+  ]);
+
+  
+  useEffect(() => {
+    
+    async function fetchCountries() {
+      const { data : fetchedCountries } = await axios.get(apiEndpoint);
+      setCountries(fetchedCountries);
+    }
+    fetchCountries();
+  }, []);
+
+  const handleAdd = async (name) => {
+    const { data: post } = await axios.post(apiEndpoint, { name: name });
+    setCountries(countries.concat(post));
+  }
+  const handleDelete = async (countryId) => {
+    const originalCountries = countries;
+    setCountries(countries.filter(c => c.id !== countryId));
+    try {
+      await axios.delete(`${apiEndpoint}/${countryId}`);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404) {
+        // country already deleted
+        console.log("The record does not exist - it may have already been deleted");
+      } else { 
+        alert('An error occurred while deleting');
+        setCountries(originalCountries);
+      }
+    }
+  }
+  const handleIncrement = (countryId, medalName) => handleUpdate(countryId, medalName, 1);
+  const handleDecrement = (countryId, medalName) =>  handleUpdate(countryId, medalName, -1)
+  const handleUpdate = async (countryId, medalName, factor) => {
+    const originalCountries = countries;
+    const idx = countries.findIndex(c => c.id === countryId);
+    const mutableCountries = [...countries ];
+    mutableCountries[idx][medalName] += (1 * factor);
+    setCountries(mutableCountries);
+    const jsonPatch = [{ op: "replace", path: medalName, value: mutableCountries[idx][medalName] }];
+    console.log(`json patch for id: ${countryId}: ${JSON.stringify(jsonPatch)}`);
+
+    try {
+      await axios.patch(`${apiEndpoint}/${countryId}`, jsonPatch);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404) {
+        // country already deleted
+        console.log("The record does not exist - it may have already been deleted");
+      } else { 
+        alert('An error occurred while updating');
+        setCountries(originalCountries);
+      }
+    }
+  }
+  const getAllMedalsTotal = () => {
+    let sum = 0;
+    medals.current.forEach(medal => { sum += countries.reduce((a, b) => a + b[medal.name], 0); });
+    return sum;
+  }
+  return (
+    <React.Fragment>
+      <div className='appHeading'>
+        Olympic Medals
+        <span className='badge'>
+          { getAllMedalsTotal() }
+        </span>
+      </div>
+      <div className='countries'>
+          { countries.map(country => 
+            <Country 
+              key={ country.id } 
+              country={ country } 
+              medals={ medals.current }
+              onDelete={ handleDelete }
+              onIncrement={ handleIncrement } 
+              onDecrement={ handleDecrement } />
+          )}
+      </div>
+      <NewCountry onAdd={ handleAdd } />
+    </React.Fragment>
+  );
+}
+ 
+export default App;
